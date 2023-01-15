4a1b93b58700f0b3e943760840025b56568ae3fe
triciahughes <hughes.a.tricia@gmail.com>
2023-01-14 16:20:37 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun("bathe my dog");
+
+function mondayWork(anything = "go to the office") {
+  return `This Monday, I will ${anything}.`;
+}
+
+function wrapAdjective(flare) {
+  if (flare === "*") {
+    return (special) => {
+      return `You are ${flare}${special}${flare}!`;
+    };
+  }
+  if (flare === "||") {
+    return (special) => {
+      return `You are ${flare}${special}${flare}!`;
+    };
+  }
+}
+//   (special) => {
+//     return `You are ${flare}${special}!${flare}`;
+//   };
+// }
+// wrapAdjective("*", "special");
